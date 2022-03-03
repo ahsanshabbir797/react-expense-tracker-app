1ffa231fda095d49dd2d1595e70c9566918ff256
ahsanshabbir797 <98451073+ahsanshabbir797@users.noreply.github.com>
2022-03-03 20:54:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
 import AddTransaction from './components/AddTransaction';
-import { GlobalContext } from './Context/GlobalState';
 import { GlobalProvider } from './Context/GlobalState';
 
 import './App.css';
